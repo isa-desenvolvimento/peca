698284e8bd60de9f7e2c1a192dbc7d92b86efc70
C1321644 Isa Andressa Novaes Menezes <c13216440@interno.bb.com.br>
2022-04-01 12:24:26 -0300

    style(welcome): ajuste

--- src/screens/Welcome.vue
+++ src/screens/Welcome.vue
@@ -4,7 +4,7 @@
       <div class="">
         <Title
           :title="$t('WELCOME')"
-          classe="text-red w-80 text-lg leading-[2.5rem]"
+          classe="text-red w-80 text-lg leading-[2.5rem]  lg:text-6xl lg:leading-[3.5rem] lg:w-[30rem]"
         />
         <Description classe="text-red text-sm  w-72 text-left  font-manrope">
           {{ $t('UHUL') }}
@@ -21,9 +21,9 @@
         <div class="flex">
           <input
             id="PASSWORD"
-            v-model="value.password"
+            v-model="value.pwd"
             type="password"
-            class="appearance-none bg-orange font-manrope border-red text-red placeholder-red text-center text-xs w-full"
+            class="appearance-none tracking-[.08em] bg-orange font-manrope border-red text-red placeholder-red text-center text-xs w-full"
             :placeholder="$t('PASSWORD')"
           />
         </div>
@@ -33,14 +33,14 @@
             v-model="value.confirm"
             :placeholder="$t('CONFIRME_PASSWORD')"
             type="password"
-            class="appearance-none bg-orange font-manrope border-red text-red placeholder-red text-center text-xs w-full"
+            class="appearance-none tracking-[.08em] bg-orange font-manrope border-red text-red placeholder-red text-center text-xs w-full"
             @change="validPassword"
           />
         </div>
         <div>
           <button
             type="submit"
-            class="appearance-none rounded-md uppercase bg-red mx-auto group relative w-20 leading-normal flex justify-center mt-8 py-2 px-16 text-xs font-manrope text-orange bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            class="appearance-none tracking-[.08em] rounded-md uppercase bg-red mx-auto group relative w-20 leading-normal flex justify-center mt-8 py-2 px-16 text-xs font-manrope text-orange bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             {{ $t('CONTINUE') }}
           </button>
@@ -53,16 +53,25 @@
 <script>
 import Description from '@/components/Description.vue'
 import Title from '@/components/Title.vue'
+
+import { useRoute } from 'vue-router'
+
 export default {
   components: { Description, Title },
+  setup() {
+    const route = useRoute()
+    const token = route.params.token || null
+
+    return { token }
+  },
   data() {
     return {
-      value: { password: '', confirm: '' },
+      value: { pwd: '', confirm: '', notAdd: true, token: this.token },
     }
   },
   methods: {
     validPassword() {
-      if (this.value.password !== this.value.confirm) {
+      if (this.value.pwd !== this.value.confirm) {
         this.$store.dispatch('form/setToast', {
           status: 'danger',
           type: 'PASSWORD_CONFIRM',
@@ -77,6 +86,8 @@ export default {
       e.preventDefault()
 
       if (this.validPassword()) {
+        delete this.value.confirm
+
         this.$store
           .dispatch('form/create', {
             value: this.value,
