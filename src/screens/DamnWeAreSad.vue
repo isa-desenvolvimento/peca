accb111067e25eb32cf1c4fe67511f87104020da
C1321644 Isa Andressa Novaes Menezes <c13216440@interno.bb.com.br>
2022-04-01 12:28:35 -0300

    style: ajuste

--- src/screens/DamnWeAreSad.vue
+++ src/screens/DamnWeAreSad.vue
@@ -1,32 +1,30 @@
 <template>
-  <translation name="slide-fade" :duration="{ enter: 500, leave: 800 }">
-    <div class="h-screen flex items-center bg-pink pl-12 lg:pl-0">
-      <div class="grid lg:justify-center lg:mx-auto">
-        <Title
-          :title="$t('POXA')"
-          classe="text-orange lg:text-center lg:ml-8"
-        />
-        <Description
-          classe="text-orange text-md  w-full text-left lg:mx-auto font-manrope  mt-[-.5rem] "
-        >
-          {{ $t('WE_ARE_SAD') }}
-        </Description>
-        <Description
-          classe="text-orange text-sm w-full text-left lg:mx-auto font-manrope mt-2"
-        >
-          <div class="w-[13rem]">
-            {{ $t('BUT_IF_YOU_CHANGE_YOUR_IDEA_IS_TO') }}
-            <a
-              href="/login"
-              class="underline underline-offset-1 font-manrope font-bold"
-            >
-              {{ $t('CLICK_HERE') }}
-            </a>
-          </div>
-        </Description>
-      </div>
+  <div class="h-screen grid items-center w-full bg-pink ease-in duration-300">
+    <div class="grid lg:justify-center lg:mx-auto ml-20">
+      <Title
+        :title="$t('POXA')"
+        classe="text-orange lg:text-center lg:ml-8 text-lg leading-[2.5rem]  lg:text-6xl lg:leading-[3.5rem] lg:w-[30rem]"
+      />
+      <Description
+        classe="text-orange text-md  w-full text-left lg:mx-auto font-manrope  mt-[-.5rem] "
+      >
+        {{ $t('WE_ARE_SAD') }}
+      </Description>
+      <Description
+        classe="text-orange text-sm w-full text-left lg:mx-auto font-manrope mt-2"
+      >
+        <div class="w-[15rem]">
+          {{ $t('BUT_IF_YOU_CHANGE_YOUR_IDEA_IS_TO') }}
+          <a
+            href="/login"
+            class="underline underline-offset-1 font-manrope font-bold"
+          >
+            {{ $t('CLICK_HERE') }}
+          </a>
+        </div>
+      </Description>
     </div>
-  </translation>
+  </div>
 </template>
 
 <script>
