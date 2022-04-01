07bf84aef2af1909fb8b2fe1a6e7def6f433c1d7
C1321644 Isa Andressa Novaes Menezes <c13216440@interno.bb.com.br>
2022-04-01 00:13:04 -0300

    style(screen): ajuste de reponsividade de tela

--- src/screens/DamnWeAreSad.vue
+++ src/screens/DamnWeAreSad.vue
@@ -1,29 +1,39 @@
 <template>
-  <feed-back
-    :title="$t('POXA')"
-    :description="$t('WE_ARE_SAD')"
-    background="bg-pink"
-  >
-    <template #body>
-      <Description
-        classe="text-orange text-sm w-full text-left mt-6 mb-60 mx-auto"
-      >
-        <div class="w-[13rem]">
-          {{ $t('BUT_IF_YOU_CHANGE_YOUR_IDEA_IS_TO') }}
-          <a href="/login" class="underline underline-offset-1">
-            {{ $t('CLICK_HERE') }}
-          </a>
-        </div>
-      </Description>
-    </template>
-  </feed-back>
+  <translation name="slide-fade" :duration="{ enter: 500, leave: 800 }">
+    <div class="h-screen flex items-center bg-pink pl-12 lg:pl-0">
+      <div class="grid lg:justify-center lg:mx-auto">
+        <Title
+          :title="$t('POXA')"
+          classe="text-orange lg:text-center lg:ml-8"
+        />
+        <Description
+          classe="text-orange text-md  w-full text-left lg:mx-auto font-manrope  mt-[-.5rem] "
+        >
+          {{ $t('WE_ARE_SAD') }}
+        </Description>
+        <Description
+          classe="text-orange text-sm w-full text-left lg:mx-auto font-manrope mt-2"
+        >
+          <div class="w-[13rem]">
+            {{ $t('BUT_IF_YOU_CHANGE_YOUR_IDEA_IS_TO') }}
+            <a
+              href="/login"
+              class="underline underline-offset-1 font-manrope font-bold"
+            >
+              {{ $t('CLICK_HERE') }}
+            </a>
+          </div>
+        </Description>
+      </div>
+    </div>
+  </translation>
 </template>
 
 <script>
-import FeedBack from '@/template/FeedBack.vue'
 import Description from '@/components/Description.vue'
+import Title from '@/components/Title.vue'
 
 export default {
-  components: { FeedBack, Description },
+  components: { Description, Title },
 }
 </script>
