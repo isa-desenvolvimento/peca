35949712440bdb74103922c4312c7237bb180950
C1321644 Isa Andressa Novaes Menezes <c13216440@interno.bb.com.br>
2022-04-04 10:30:18 -0300

    feat(term): ajuste de termo

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import VueAxios from 'vue-axios'
 import VueTheMask from 'vue-the-mask'
 import moshaToast from 'mosha-vue-toastify'
 import Popper from 'vue3-popper'
+import Markdown from 'vue3-markdown-it'
 
 import App from './App.vue'
 import router from './router'
@@ -13,11 +14,13 @@ import './index.css'
 
 import 'mosha-vue-toastify/dist/style.css'
 import 'tw-elements'
+import 'highlight.js/styles/monokai.css'
 
 createApp(App)
   .use(router)
   .use(store)
   .use(moshaToast)
+  .use(Markdown)
   .component('Popper', Popper)
   .use(VueTheMask)
   .use(VueAxios, axios)
--- src/screens/TermOfUse.vue
+++ src/screens/TermOfUse.vue
@@ -12,75 +12,7 @@
           class="font-manrope border text-red font-light text-sm break-words p-2 overflow-auto mb-20"
         >
           <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-            fugiat iusto fuga praesentium optio, eaque rerum! Provident
-            similique accusantium nemo autem. Veritatis obcaecati tenetur iure
-            eius earum ut molestias architecto voluptate aliquam nihil, eveniet
-            aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur
-            error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias
-            eos sapiente officiis modi at sunt excepturi expedita sint? Sed
-            quibusdam recusandae alias error harum maxime adipisci amet laborum.
-            Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates
-            a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius
-            fugit doloribus tenetur fugiat, temporibus enim commodi iusto libero
-            magni deleniti quod quam consequuntur! Commodi minima excepturi
-            repudiandae velit hic maxime doloremque. Quaerat provident commodi
-            consectetur veniam similique ad earum omnis ipsum saepe, voluptas,
-            hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam
-            cupiditate excepturi mollitia maiores labore suscipit quas? Nulla,
-            placeat. Voluptatem quaerat non architecto ab laudantium modi minima
-            sunt esse temporibus sint culpa, recusandae aliquam numquam totam
-            ratione voluptas quod exercitationem fuga. Possimus quis earum
-            veniam quasi aliquam eligendi, placeat qui corporis! Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-            molestiae quas vel sint commodi repudiandae consequuntur voluptatum
-            laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto
-            fuga praesentium optio, eaque rerum! Provident similique accusantium
-            nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias
-            architecto voluptate aliquam nihil, eveniet aliquid culpa officia
-            aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt
-            ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque error
-            repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis
-            modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias
-            error harum maxime adipisci amet laborum. Perspiciatis minima
-            nesciunt dolorem! Officiis iure rerum voluptates a cumque velit
-            quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus
-            tenetur fugiat, temporibus enim commodi iusto libero magni deleniti
-            quod quam consequuntur! Commodi minima excepturi repudiandae velit
-            hic maxime doloremque. Quaerat provident commodi consectetur veniam
-            similique ad earum omnis ipsum saepe, voluptas, hic voluptates
-            pariatur est explicabo fugiat, dolorum eligendi quam cupiditate
-            excepturi mollitia maiores labore suscipit quas? Nulla, placeat.
-            Voluptatem quaerat non architecto ab laudantium modi minima sunt
-            esse temporibus sint culpa, recusandae aliquam numquam totam ratione
-            voluptas quod exercitationem fuga. Possimus quis earum veniam quasi
-            aliquam eligendi, placeat qui corporis! Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Maxime mollitia, molestiae quas vel
-            sint commodi repudiandae consequuntur voluptatum laborum numquam
-            blanditiis harum quisquam eius sed odit fugiat iusto fuga
-            praesentium optio, eaque rerum! Provident similique accusantium nemo
-            autem. Veritatis obcaecati tenetur iure eius earum ut molestias
-            architecto voluptate aliquam nihil, eveniet aliquid culpa officia
-            aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt
-            ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque error
-            repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis
-            modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias
-            error harum maxime adipisci amet laborum. Perspiciatis minima
-            nesciunt dolorem! Officiis iure rerum voluptates a cumque velit
-            quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus
-            tenetur fugiat, temporibus enim commodi iusto libero magni deleniti
-            quod quam consequuntur! Commodi minima excepturi repudiandae velit
-            hic maxime doloremque. Quaerat provident commodi consectetur veniam
-            similique ad earum omnis ipsum saepe, voluptas, hic voluptates
-            pariatur est explicabo fugiat, dolorum eligendi quam cupiditate
-            excepturi mollitia maiores labore suscipit quas? Nulla, placeat.
-            Voluptatem quaerat non architecto ab laudantium modi minima sunt
-            esse temporibus sint culpa, recusandae aliquam numquam totam ratione
-            voluptas quod exercitationem fuga. Possimus quis earum veniam quasi
-            aliquam eligendi, placeat qui corporis!
+            <Markdown :source="termo" />
           </div>
         </div>
 
@@ -98,5 +30,22 @@
 </template>
 
 <script>
-export default {}
+import Markdown from 'vue3-markdown-it'
+import { useStore } from 'vuex'
+
+export default {
+  components: {
+    Markdown,
+  },
+  setup() {
+    const { dispatch } = useStore()
+
+    dispatch('list/getList', 'termo')
+  },
+  computed: {
+    termo() {
+      return this.$store.state.list?.termo
+    },
+  },
+}
 </script>
