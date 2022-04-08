b99f182eae8dd95a606234653042bdab2661de55
C1321644 Isa Andressa Novaes Menezes <c13216440@interno.bb.com.br>
2022-04-08 14:42:19 -0300

    fix: valor da loja

--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -14,7 +14,7 @@
           v-if="balance_on_visible"
           class="col-span-4 col-start-3 lg:col-start-2 text-left text-yellow font-manrope"
         >
-          {{ `R$ ${balance?.disponivel}` }}
+          {{ `R$ ${balance?.disponivel || '0,00'}` }}
         </span>
         <span v-else class="col-span-1 text-left text-yellow">-</span>
       </div>
@@ -34,7 +34,7 @@
           v-if="balance_off_visible"
           class="col-span-4 lg:col-start-8 text-left text-yellow font-manrope"
         >
-          {{ `R$ ${balance?.bloqueado}` }}
+          {{ `R$ ${balance?.bloqueado || '0,00'}` }}
         </span>
         <span v-else class="col-span-1 text-left text-yellow">-</span>
       </div>
