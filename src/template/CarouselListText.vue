4ad1869ba7a90e93bfe3439ed3b212a256656083
C1321644 Isa Andressa Novaes Menezes <c13216440@interno.bb.com.br>
2022-04-08 10:30:31 -0300

    fix: ajuste de icone de showmore

--- src/template/CarouselListText.vue
+++ src/template/CarouselListText.vue
@@ -5,9 +5,10 @@
     >
       <Header :title="title" />
 
-      <div class="row-span-6 my-8 md:w-6/12 md:mx-auto relative">
+      <div class="row-span-6 my-8 md:w-6/12 md:mx-auto">
         <Carousel
           v-if="hasCarousel"
+          id="jump_to_me"
           :slides="lojas"
           icon="bg-icon-marcador-menu"
           :onclick="(slide) => setId(slide)"
@@ -16,7 +17,12 @@
         />
 
         <hr v-if="hasCarousel" class="col-span-2 text-yellow opacity-20 my-4" />
-        <div v-for="(list, index) in lists" :key="index">
+        <div
+          v-for="(list, index) in lists"
+          :key="index"
+          class="relative"
+          :class="showmore ? 'overflow-y-auto' : 'overflow-y-hidden  h-96'"
+        >
           <span
             v-if="lists?.movimentos.length"
             class="text-red text-sm font-bold font-manrope"
@@ -27,18 +33,39 @@
             {{ $t('THERE_ARE_NO_PRODUCTS') }}
           </span>
 
-          <List :lists="list" :onclick="setItem" />
-        </div>
+          <div class="pb-8">
+            <List :lists="list" :onclick="setItem" />
+          </div>
 
-        <div
-          v-if="estoque == 1"
-          class="mt-8 cursor-pointer"
-          @click="$router.push(`/product/${id_loja}`)"
-        >
-          <span class="text-red text-sm font-bold">
-            {{ $t('SOLD_RETURNED_CANCELED') }}
-          </span>
+          <a v-if="hasShowMore" href="#jump_to_me">
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              class="animate-bounce m-auto w-10 h-10 p-2 bg-yellow rounded-full text-white text-center absolute inset-x-0 bottom-0"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              @click="showmore = !showmore"
+            >
+              <path
+                class="origin-center"
+                :class="!showmore ? 'rotate-180' : ''"
+                fill="currentColor"
+                d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"
+              ></path>
+            </svg>
+          </a>
         </div>
+      </div>
+      <div
+        v-if="estoque == 1"
+        class="mt-8 cursor-pointer"
+        @click="$router.push(`/product/${id_loja}`)"
+      >
+        <span class="text-red text-sm font-bold">
+          {{ $t('SOLD_RETURNED_CANCELED') }}
+        </span>
       </div>
     </div>
   </translation>
@@ -79,12 +106,27 @@ export default {
 
     return { id_loja, dispatch }
   },
+  data() {
+    return { showmore: false }
+  },
   computed: {
     lojas() {
       return this.$store.state.list?.lojas
     },
     lists() {
       return this.$store.state.list[this.type]
+    },
+
+    hasShowMore() {
+      if (
+        this.lists?.movimentos.length > 1 ||
+        this.lists?.movimentos[0].periodos.length > 2 ||
+        this.lists?.movimentos[0].periodos[0].itens.length > 2
+      ) {
+        return true
+      }
+
+      return false
     },
   },
   mounted() {
