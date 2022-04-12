4559e1e7183f7595e1779d0f04f9abd2e80d5560
C1321644 Isa Andressa Novaes Menezes <c13216440@interno.bb.com.br>
2022-04-11 23:31:21 -0300

    fix: ajuste de loading

--- src/store/list.module.js
+++ src/store/list.module.js
@@ -9,32 +9,26 @@ export const list = {
   state: initialState,
   actions: {
     getLojas({ commit }, type) {
-      this.dispatch('form/setLoading')
       return getLojas(type).then(
         (payload) => {
           commit('sucess', { payload: payload.data, type: 'lojas' })
-          this.dispatch('form/setLoading')
 
           return Promise.resolve(payload.data)
         },
         (error) => {
           commit('failure')
-          this.dispatch('form/setLoading')
           return Promise.reject(error)
         }
       )
     },
     getList({ commit }, type) {
-      this.dispatch('form/setLoading')
       return getList(type).then(
         (payload) => {
           commit('sucess', { payload: payload.data, type })
-          this.dispatch('form/setLoading')
           return Promise.resolve(payload.data)
         },
         (error) => {
           commit('failure')
-          this.dispatch('form/setLoading')
           return Promise.reject(error)
         }
       )
@@ -52,35 +46,30 @@ export const list = {
       )
     },
     getFilter({ commit }, value) {
-      this.dispatch('form/setLoading')
       this.state.list[value.type] = {}
       return getFilter(value).then(
         (payload) => {
           commit('sucess', { payload: payload.data, type: value.type })
-          this.dispatch('form/setLoading')
 
           return Promise.resolve(payload.data)
         },
         (error) => {
           commit('failure')
-          this.dispatch('form/setLoading')
+
           return Promise.reject(error)
         }
       )
     },
     getListEstoque({ commit }, value) {
-      this.dispatch('form/setLoading')
-
       return getListEstoque(value).then(
         (payload) => {
           commit('sucess', { payload: payload.data, type: value.type })
-          this.dispatch('form/setLoading')
 
           return Promise.resolve(payload.data)
         },
         (error) => {
           commit('failure')
-          this.dispatch('form/setLoading')
+
           return Promise.reject(error)
         }
       )
