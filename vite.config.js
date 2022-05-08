e19f596c65b62aee8d7c2b2210f9f88085471f08
Lindolfo Junior <38046370+LindolfoJunior@users.noreply.github.com>
2022-05-08 10:34:57 -0300

    Ajustes para inclusão dos favicons e também das imagens

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,12 @@ import { VitePWA } from 'vite-plugin-pwa'
 const pwaOptions = {
   mode: 'production',
   base: '/',
-  includeAssets: ['favicon.ico'],
+  includeAssets: [
+	'favicon.ico',
+	'.htaccess',
+	'favicons',
+	'img'
+  ],
   manifest: {
     short_name: 'Clube Peça Rara',
     name: 'Clube Peça Rara',
@@ -22,57 +27,105 @@ const pwaOptions = {
     start_url: '/',
     display: 'standalone',
     orientation: 'natural',
+    related_applications: [],
+	  prefer_related_applications: false,
+    features: [
+		  "Cross Platform",
+		  "low-latency inking",
+		  "fast",
+		  "useful AI"
+    ],
     icons: [
       {
-        src: '/assets/favicon/android-icon-36x36.png',
+        src: '/favicons/android-icon-36x36.png',
         sizes: '36x36',
         type: 'image/png',
         density: '0.75',
+		purpose: "any"
       },
       {
-        src: '/assets/favicon/android-icon-48x48.png',
+        src: '/favicons/android-icon-48x48.png',
         sizes: '48x48',
         type: 'image/png',
         density: '1.0',
+		purpose: "any"
       },
       {
-        src: '/assets/favicon/android-icon-72x72.png',
+        src: '/favicons/android-icon-72x72.png',
         sizes: '72x72',
         type: 'image/png',
         density: '1.5',
+		purpose: "any"
       },
       {
-        src: '/assets/favicon/android-icon-96x96.png',
+        src: '/favicons/android-icon-96x96.png',
         sizes: '96x96',
         type: 'image/png',
         density: '2.0',
+		purpose: "any"
       },
       {
-        src: '/assets/favicon/android-icon-144x144.png',
+        src: '/favicons/android-icon-144x144.png',
         sizes: '144x144',
         type: 'image/png',
         density: '3.0',
+		purpose: "any"
       },
       {
-        src: '/assets/favicon/android-icon-192x192.png',
+        src: '/favicons/android-icon-152x152.png',
+        sizes: '152x152',
+        type: 'image/png',
+        density: '3.5',
+		purpose: "any"
+      },
+      {
+        src: '/favicons/android-icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
         density: '4.0',
-        purpose: 'any maskable',
+        purpose: 'any',
+      },
+      {
+        src: '/favicons/android-icon-384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+        density: '4.5',
+        purpose: 'any',
+      },
+      {
+        src: '/favicons/android-icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        density: '4.5',
+        purpose: 'any',
       },
     ],
     screenshots: [
       {
-        src: '/assets/img/screenshoot01.png',
+        src: '/img/screenshoot01.png',
         type: 'image/png',
         sizes: '540x720',
       },
       {
-        src: '/assets/img/screenshoot02.png',
+        src: '/img/screenshoot02.png',
         type: 'image/jpg',
         sizes: '540x720',
       },
     ],
+    shortcuts: [
+		{
+			name: 'Clube Peça Rara',
+			short_name: 'Clube Peça Rara',
+			description: 'Sistema de Gestão de Fornecedores',
+			url: "/",
+			icons: [
+				{
+					src: '/favicons/android-icon-192x192.png',
+					sizes: '192x192'
+				}
+			]
+		}
+	],
   },
   devOptions: {
     enabled: process.env.SW_DEV === 'true',
